c5cef0702903b10a9af002f1a06148c698c33d40
FrancoStoll <francostoll@gmail.com>
2024-04-19 23:20:31 -0300

    improve from validation next

--- validacion.js
+++ validacion.js
@@ -1,1 +1,16 @@
-//Haz tú validación en javascript acá
+const formulario = document.querySelector('#contacto');
+const nombre = document.querySelector('input[name="nombre"]');
+const email = document.querySelector('input[name="email"]');
+const asunto = document.querySelector('input[name="asunto"]');
+
+formulario.addEventListener('submit', enviarFormulario);
+
+function enviarFormulario(e) {
+  e.preventDefault();
+
+  if ([nombre.value, email.value, asunto.value].includes('')) {
+    return alert('Completa todos los campos');
+  }
+
+  return alert('Enviado');
+}
